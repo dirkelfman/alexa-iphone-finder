2417c7418f0efb9e07499368eecc27f56ad9b5da
dirkelfman@gmail.com <bitbucketpw1>
2016-05-21 10:34:19 -0500

    doing stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,18 +4,42 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     lambda_package: {
-        default: {
-            options: {
-                // Task-specific options go here.
-            }
-        }
-    },
-  });
+      default: {
+        options: {
+          include_files: [
+            'config.json',
+            './config.json',
 
+          ]
+        // Task-specific options go here.
+        }
+      }
+    },
+    lambda_deploy: {
+      default: {
+        arn: 'arn:aws:lambda:us-east-1:663361677957:function:iphone-finder-skill',
+        options: {
+          // Task-specific options go here.
+        }
+      }
+    },
+    clean: {
+      build: ["dist"]
+    },
+    jshint: {
+      all: ['Gruntfile.js', 'app.js', '_testdriver.js' ]
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-aws-lambda');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  
   // Load the plugin that provides the "uglify" task.
- 
+
   // Default task(s).
-  grunt.registerTask('default', ['lambda_package']);
+  grunt.registerTask('default', ['jshint']);
+
+  grunt.registerTask('deploy', ['default', 'clean', 'lambda_package', 'lambda_deploy']);
 
 };
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 ﻿/**
  * App ID for the skill
  */
-var APP_ID = null; //replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-here]';
+var APP_ID = null; //replace with 'amzn1.183-sdk-ams.app.[your-unique-value-here]';
 
 //ARN - arn:aws:lambda:us-east-1:663361677957:function:Kodi_Skill
 
@@ -9,18 +9,16 @@ var APP_ID = null; //replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-her
  * The AlexaSkill prototype and helper functions
  */
 var AlexaSkill = require('./AlexaSkill');
-var FindMyIphone = require("find-my-iphone")
-var soundex = require('soundex')
+var FindMyIphone = require("find-my-iphone");
+var soundex = require('soundex');
 
-var myLatitude = 30.541152;
-var myLongitude = -97.740255;
+
 
 var appConfig = null;
 try {
-    appConfig = require('./config');
-}
-catch (e) {
-    throw new Error('needs ./config.json in the format of { "username": "foo@boo.com", "password": "password1" }');
+  appConfig = require('./config');
+} catch (e) {
+  throw new Error('needs ./config.json in the format of { "username": "foo@boo.com", "password": "password1" }');
 }
 
 
@@ -33,8 +31,8 @@ catch (e) {
      *
      * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
      */
-var FindIphoneSkill = function () {
-    AlexaSkill.call(this, APP_ID);
+var FindIphoneSkill = function() {
+  AlexaSkill.call(this, APP_ID);
 };
 
 // Extend AlexaSkill
@@ -44,190 +42,214 @@ FindIphoneSkill.prototype.constructor = FindIphoneSkill;
 /**
  * Overriden to show that a subclass can override this function to initialize session state.
  */
-FindIphoneSkill.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-    console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId + ", sessionId: " + session.sessionId);
+FindIphoneSkill.prototype.eventHandlers.onSessionStarted = function(sessionStartedRequest, session) {
+  console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId + ", sessionId: " + session.sessionId);
 
-    // Any session init logic would go here.
+// Any session init logic would go here.
 };
 
 /**
  * If the user launches without specifying an intent, route to the correct function.
  */
-FindIphoneSkill.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    console.log("FindIphoneSkill onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
-    
-    var speechOutput = " you can ask to beep someone or how far away is.";
-    
-    
-    response.ask(speechOutput);
+FindIphoneSkill.prototype.eventHandlers.onLaunch = function(launchRequest, session, response) {
+  console.log("FindIphoneSkill onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+
+  var spe1utput = " you can ask to beep someone or how far away is.";
+
+
+  response.ask(speechOutput);
 };
 
 /**
  * Overriden to show that a subclass can override this function to teardown session state.
  */
-FindIphoneSkill.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-    console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId + ", sessionId: " + session.sessionId);
+FindIphoneSkill.prototype.eventHandlers.onSessionEnded = function(sessionEndedRequest, session) {
+  console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId + ", sessionId: " + session.sessionId);
 
-    //Any session cleanup logic would go here.
+//Any session cleanup logic would go here.
 };
 
 
 FindIphoneSkill.prototype.intentHandlers = {
-    ListIntent: function (intent, session, response) {
-        handleListIntent(response);
-    },
-    
-    BeepIntent: function (intent, session, response) {
-        handleBeepIntent(intent , response);
-    },
-    HowFarIntent: function (intent, session, response) {
-        handleHowFarIntent(intent , response);
-    },
-   
-    
-    HelpIntent: function (intent, session, response) {
-        var speechOutput = " you can ask to play  or pause or search.  you stupid idiot.";
-        
-        
-        response.ask(speechOutput);
-    }
+  ListIntent: function(intent, session, response) {
+    handleListIntent(response);
+  },
+
+  BeepIntent: function(intent, session, response) {
+    handleBeepIntent(intent, response);
+  },
+  HowFarIntent: function(intent, session, response) {
+    handleHowFarIntent(intent, response);
+  },
+
+
+  HelpIntent: function(intent, session, response) {
+    var speechOutput = " you can ask to play  or pause or search.  you stupid idiot.";
+
+
+    response.ask(speechOutput);
+  }
 };
 
 function getValueFromIntentOrSession(key, intent, session) {
-    var ret = intent.slots[key] && intent.slots[key].value;
-    return ret || session[key];
+  var ret = intent.slots[key] && intent.slots[key].value;
+  return ret || session[key];
 }
 
-function getIphoneFinder(){
-    var icloud = FindMyIphone.findmyphone;
-    icloud.apple_id = appConfig.username;
-    icloud.password = appConfig.password;
-    return icloud;
+function getIphoneFinder() {
+  var icloud = FindMyIphone.findmyphone;
+  icloud.apple_id = appConfig.username;
+  icloud.password = appConfig.password;
+  return icloud;
 }
 
 function handleListIntent(response) {
-    
-    var iphoneFinder = getIphoneFinder();
-    iphoneFinder.getDevices( function ( error, devices ){
-        if ( error ){
-            response.tellWithCard( 'bad stuff happened.  View card for info ', 'iphone finder error', error.toString() );
-            return;
-        }
 
-        var devideNames = devices.filter( function(d){ return d && d.location && d.lostModeCapable }).map ( function ( d){ return d.name;});
-
-        if ( devideNames.length )
-        {
-            response.tell ('the following phones are turned on.  ' + devideNames.join ( ' and ') );
-        }else{
-            response.tell ('no devices found' );
-        }
-        
-    });
-}
-
-function isMatch ( device, personName, deviceType ){
-    personName =  (personName || '').toLowerCase();
-    deviceType =  (deviceType|| '').toLowerCase();
-
-    if ( personName.endsWith( 's')){
-        personName = personName.substring( 1, personName.length -1);
+  var iphoneFinder = getIphoneFinder();
+  iphoneFinder.getDevices(function(error, devices) {
+    if (error) {
+      response.tellWithCard('bad stuff happened.  View card for info ', 'iphone finder error', error.toString());
+      return;
     }
 
-    var personMatch = !personName  || 
-        soundex( device.name ) == soundex( personName)||
-        ( device.name.toLowerCase().indexOf( personName ) > -1 ) ||
-        soundex( device.deviceDisplayName ) == soundex( personName)||
-        ( device.deviceDisplayName.toLowerCase().indexOf( personName ) > -1  );
-    var deviceMatch = !deviceType ||  
-        soundex( device.modelDisplayName ) == soundex( deviceType)||
-         soundex( device.deviceDisplayName ) == soundex( deviceType)||
-        ( device.modelDisplayName.toLowerCase().indexOf( deviceType ) > -1 ) ||
-        ( device.deviceDisplayName.toLowerCase().indexOf( deviceType ) > -1  );
+    var devideNames = devices.filter(function(d) {
+      return d && d.location && d.lostModeCapable;
+    }).map(function(d) {
+      return d.name;
+    });
 
-   // console.log( device.name , personMatch && deviceMatch && device.lostModeCapable );
-     return personMatch && deviceMatch && device.location && device.lostModeCapable 
+    if (devideNames.length) {
+      response.tell('the following phones are turned on.  ' + devideNames.join(' and '));
+    } else {
+      response.tell('no devices found');
+    }
+
+  });
 }
 
-function handleBeepIntent( intent, response){
-  var personName = intent.slots.PersonName;
-  var deviceType= intent.slots.DeviceType;
+function isMatch(device, personName, deviceType) {
+  personName = (personName || '').toLowerCase();
+  deviceType = (deviceType || 'iphone').toLowerCase();
+
+  if (personName.endsWith('s')) {
+    personName = personName.substring(1, personName.length - 1);
+  }
+
+  var personMatch = !personName ||
+    soundex(device.name) == soundex(personName) ||
+    (device.name.toLowerCase().indexOf(personName) > -1) ||
+    soundex(device.deviceDisplayName) == soundex(personName) ||
+    (device.deviceDisplayName.toLowerCase().indexOf(personName) > -1);
+  var deviceMatch = soundex(device.modelDisplayName) == soundex(deviceType) ||
+    soundex(device.deviceDisplayName) == soundex(deviceType) ||
+    (device.modelDisplayName.toLowerCase().indexOf(deviceType) > -1) ||
+    (device.deviceDisplayName.toLowerCase().indexOf(deviceType) > -1);
+
+
+  return personMatch && deviceMatch && device.location && device.lostModeCapable;
+}
+
+function getDevice(intent, response) {
+  return new Promise(function(resolve, reject) {
+    var personName = intent.slots.PersonName;
+    var deviceType = intent.slots.DeviceType;
     if (!personName || !personName.value) {
-        response.ask('what device', 'what device name  dammit!!');
-        return;
+
+      return resolve(function() {
+        response.ask('what device', 'who now?');
+      });
+
     }
 
-     var iphoneFinder = getIphoneFinder();
-    iphoneFinder.getDevices( function ( error, devices ){
-        if ( error ){
-            response.tellWithCard( 'bad stuff happened.  View card for info ', 'iphone finder error', error.toString() );
-            return;
-        }
+    console.log('slot info ', personName, deviceType);
 
+    var iphoneFinder = getIphoneFinder();
 
-        var devices = devices.filter( function(d){ 
-            var res = isMatch( d, personName.value, deviceType.value); 
-            console.log( d.name, res );
-            return  res;
+    iphoneFinder.getDevices(function(error, devices) {
+      if (error) {
+        resolve(function() {
+          response.tellWithCard('bad stuff happened.  View card for info ', 'iphone finder error', error.toString());
         });
 
+      }
 
-        if ( devices.length ==0  )
-        {
-            response.tell ('no devices found' );
-            return ;
-        }
+      devices = devices.filter(function(d) {
+        var res = isMatch(d, personName.value, deviceType.value);
+        console.log(d.name, res);
+        return res;
+      });
 
-        iphoneFinder.alertDevice(devices[0].id, function(err) {
-              response.tell ('sending beep beep to ' + devices[0].name );
-              return;
+      if (devices.length === 0) {
+        return resolve(function() {
+          response.tellWithCard('no devices found', 'personname: ' + personName.value + ' | devicename: ' + deviceType.value);
         });
-        
+      }
+      resolve(devices[0]);
     });
 
+  });
 }
 
-function handleHowFarIntent( intent, response ){
-     if (!personName || !personName.value) {
-        response.ask('what device', 'what device name  dammit!!');
-        return;
+function handleBeepIntent(intent, response) {
+  getDevice(intent, response).then(function(res) {
+    if (typeof x === "function") {
+      x();
+      return;
+    }
+    var iphoneFinder = getIphoneFinder();
+    var device = res;
+    iphoneFinder.alertDevice(device.id, function(err) {
+      response.tell('sending beep beep to ' + device.name);
+      return;
+    });
+
+  }
+  );
+}
+
+function handleHowFarIntent(intent, response) {
+
+  getDevice(intent, response).then(function(device) {
+    if (typeof x === "function") {
+      x();
+      return;
     }
 
-     var iphoneFinder = getIphoneFinder();
-     iphoneFinder.getDevices( function ( error, devices ){
-        if ( error ){
-            response.tellWithCard( 'bad stuff happened.  View card for info ', 'iphone finder error', error.toString() );
-            return;
+    var iphoneFinder = getIphoneFinder();
+
+    iphoneFinder.getLocationOfDevice(device, function(err, location) {
+      var res = "";
+      var prefix = intent.slots.PersonName.value + ' is ';
+      if (err) {
+        console.error(err);
+        response.tell('error getting location ');
+        return;
+      }
+      res = " at " + location;
+      if (!appConfig.latitude || !appConfig.longitude) {
+        return response.tell(prefix + res);
+      }
+
+      iphoneFinder.getDistanceOfDevice(device, appConfig.latitude, appConfig.longitude, function(err, result) {
+        if (!err) {
+          res = result.distance.text + ' ' + result.duration.text + ' away ' + res;
         }
-
-
-       var devices = devices.filter( function(d){ 
-            return isMatch( d, personName.value, deviceType.value); 
-        });
-
-        if ( devices.length ==0  )
-        {
-            response.tell ('no devices found' );
-            return ;
-        }
-
-        iphoneFinder.getDistanceOfDevice(devices[0].id, myLatitude, myLongitude, function(err, result) {
-                console.log("Distance: " + result.distance.text);
-                console.log("Driving time: " + result.duration.text);
-            });
-        iphoneFinder.alertDevice(devices[0].id, function(err) {
-              response.tell ('sending beep beep to ' + devices[0].name );
-              return;
-        });
-        
+        return response.tell(prefix + res);
+      });
     });
+
+
+
+
+
+  });
 }
 
 
 
 // Create the handler that responds to the Alexa Request.
-exports.handler = function (event, context) {
-    // Create an instance of the WiseGuy Skill.
-    var skill = new FindIphoneSkill();
-    skill.execute(event, context);
+exports.handler = function(event, context) {
+  // Create an instance of the WiseGuy Skill.
+  var skill = new FindIphoneSkill();
+  skill.execute(event, context);
 };
